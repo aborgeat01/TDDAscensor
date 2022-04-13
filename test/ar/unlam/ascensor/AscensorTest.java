14b121034b1b7debac33bcf832730dbefefaf21b
aborgeat01 <you@example.com>
2022-04-13 09:58:42 -0300

    first commit

---
+++ test/ar/unlam/ascensor/AscensorTest.java
@@ -0,0 +1,109 @@
+package ar.unlam.ascensor;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.Test;
+
+public class AscensorTest {
+
+
+	@Test
+	public void queSePuedaCrearUnAscensorConNombrePisoMaximoPisoMinimoPisoActualEn0CantidadDePersonasYPesoMaximo() {
+		
+	}
+	
+	
+	
+	@Test
+	public void queSePuedaSubirUnaPersonaAlAcensor( )  {
+		
+	}
+	
+	@Test
+	public void queSePuedaBajarUnaPersonadelAcensor( )  {
+		
+	}
+
+	@Test
+	public void queSePuedaObteneLaCantidadDePersonasQueHayAcensor( )  {
+		
+	}
+	
+
+	@Test
+	public void queSePuedaCalcularLaSumatoriaDeTodasLAsPersonasQueHayAcensor( )  {
+		
+	}
+
+	@Test
+	public void queCuandoUnAscensorSeCreaPuedaIraUnPiso() {
+		
+	}
+	
+	
+	@Test
+	public void queCuandoSeQuieraIrAUnPisoMayorAlPisoMaximoElAscensorPermanezcaEnELPisoActual( )  {
+		
+	}
+	
+
+	@Test
+	public void queCuandoSeQuieraIrAUnPisoMenorAlPisoMinimoElAscensorPermanezcaEnELPisoActual( )  {
+		
+	}
+	
+	@Test
+	public void queSePuedaCerrarUnaPuerta( )  {
+		
+	}
+	
+	
+	@Test
+	public void queSePuedaAbrirUnaPuerta( )  {
+		
+	}
+	
+	@Test
+	public void queCuandoLaPuertaEsteAbiertaNoPuedaIrAlPisoDseadoYpermenezcaEnELPisoActual( )  {
+		
+	}
+	
+	
+	@Test
+	public void queCuandoLaPuertaEsteCerradaElAscensorVayaAlPisoDeseado( )  {
+		
+	}
+	
+
+	
+	
+	
+	@Test
+	public void queNoSePuedaSubirUnaPersonaAlAscensorSiSuPeraLaCantidadDePersonasMaxinasPermitidas( )  {
+		
+	}
+	
+	@Test
+	public void queNoSePuedaSubirUnaPersonaAlAscensorSiSuPeraELPesoMaximoPermotidoDelAscensor( )  {
+		
+	}
+	
+	@Test
+	public void queNoSePuedaSubirUnaPersonaAlAscensorSiLaPuertaEstaCerrada () {
+		
+	}
+	
+
+	@Test
+	public void queNoSePuedaBajarUnaPersonaAlAscensorSiLaPuertaEstaCerrada () {
+		
+	}
+	
+	
+	
+	
+	
+	
+
+}
